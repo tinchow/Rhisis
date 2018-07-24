f599adbeae0c0964b8697898dc1d37fbc3bc52d8
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2018-07-24 08:50:35 +0200

    Add battle handler and battle system base

---
+++ src/Rhisis.World/Handlers/BattleHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ether.Network.Packets;
+using Rhisis.Core.IO;
+using Rhisis.Core.Network;
+using Rhisis.Core.Network.Packets;
+using Rhisis.World.Systems.Battle;
+
+namespace Rhisis.World.Handlers
+{
+    internal static class BattleHandler
+    {
+        [PacketHandler(PacketType.MELEE_ATTACK)]
+        public static void OnMeleeAttack(WorldClient client, INetPacketStream packet)
+        {
+            var attackMessage = packet.Read<int>();
+            var objectId = packet.Read<int>();
+            var unused = packet.Read<int>(); // Always 0
+            var error = packet.Read<int>();
+            var attackSpeed = packet.Read<float>();
+
+            Logger.Debug("Player {0} attacks | {1}, {2}, {3}, {4}, {5}", 
+                client.Player.Object.Name, attackMessage, objectId, unused, error, attackSpeed);
+
+            // TODO: create event args for melee attack
+            client.Player.NotifySystem<BattleSystem>(null);
+        }
+    }
+}
---
+++ src/Rhisis.World/Systems/Battle/BattleSystem.cs
@@ -0,0 +1,18 @@
+﻿using Rhisis.World.Game.Core;
+
+namespace Rhisis.World.Systems.Battle
+{
+    [System]
+    public sealed class BattleSystem : NotifiableSystemBase
+    {
+        public BattleSystem(IContext context) 
+            : base(context)
+        {
+        }
+
+        public override void Execute(IEntity entity, SystemEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
